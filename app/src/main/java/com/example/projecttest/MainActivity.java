aa1e9fdc308887b172370d751075289e96ed199b
lyubc <lyubckina00@gmail.com>
2023-04-23 13:25:16 +0500

    h

--- app/src/main/java/com/example/projecttest/MainActivity.java
+++ app/src/main/java/com/example/projecttest/MainActivity.java
@@ -3,12 +3,10 @@ package com.example.projecttest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 }
